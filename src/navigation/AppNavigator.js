58d444da4c0d48612a31cd80353528330151e654
AamaeyPrakash <aamaeyprakash@gmail.com>
2024-11-26 14:37:41 +0530

    bottomtab icons

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Image} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -10,17 +11,66 @@ import MathQuizScreen from '../screens/MathQuizScreen';
 import TopicCardAI from '../TopicCardAI';
 import HomeScreen from '../screens/HomeScreen';
 import ProfileScreen from '../screens/ProfileScreen';
+import homeIcon from '../assets/homeIcon.png'
+import askIcon from '../assets/askIcon.png'
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
+const extraTabOptions = {
+  tabBarLabelStyle: { fontFamily: 'exo' },
+  tabBarStyle: { borderTopRightRadius: 12, borderTopLeftRadius: 12 },
+  tabBarActiveTintColor:'#1C4E80',
+  tabBarInactiveTintColor: '#364356',
+};
+
 
 const TabNavigator = () => {
   return (
     <Tab.Navigator>
-        <Tab.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
-        <Tab.Screen name="PromptScreen" component={PromptScreen} options={{ headerShown: false }}/>
-        <Tab.Screen name="ProfileScreen" component={ProfileScreen} options={{ headerShown: false }}/>
-        <Tab.Screen name="Sign In" component={SignInScreen}/>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon(props) {
+            return (
+              <Image
+                source={homeIcon}
+                style={{
+                  tintColor: props.color,
+                  width: props.size,
+                  height: props.size,
+                }}
+                {...props}
+              />
+            );
+          },
+          ...extraTabOptions,
+        }}
+      />
+            <Tab.Screen
+        name="AskGPT"
+        component={PromptScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon(props) {
+            return (
+              <Image
+                source={askIcon}
+                style={{
+                  tintColor: props.color,
+                  width: props.size,
+                  height: props.size,
+                }}
+                {...props}
+              />
+            );
+          },
+          ...extraTabOptions,
+        }}
+      />
+      <Tab.Screen name="ProfileScreen" component={ProfileScreen} options={{ headerShown: false }}/>
+      <Tab.Screen name="Sign In" component={SignInScreen}/>
     </Tab.Navigator>
   );
 };
@@ -34,16 +84,30 @@ const AppNavigator = () => {
           component={TabNavigator}
           options={{ headerShown: false }} 
         />
-        <Stack.Screen name="Math AA" component={TopicCardAA} />
-        <Stack.Screen name="Math AI" component={TopicCardAI} />
-        <Stack.Screen name="QuizScreen" component={MathQuizScreen} />
+        <Stack.Screen 
+          name="Math AA" 
+          component={TopicCardAA}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Math AI"
+          component={TopicCardAI}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen 
+          name="QuizScreen" 
+          component={MathQuizScreen} 
+          options={{ headerShown: false }}
+        />
         <Stack.Screen
           name="SignUp"
           component={SignUpScreen}
+          options={{ headerShown: false }}
         />
         <Stack.Screen
           name="SignIn"
           component={SignInScreen}
+          options={{ headerShown: false }}
         />
       </Stack.Navigator>
     </NavigationContainer>
