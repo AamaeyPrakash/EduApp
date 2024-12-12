1bfac4add07a1f6c036f893022d0d4dbefc072b2
AamaeyPrakash <aamaeyprakash@gmail.com>
2024-12-12 17:37:00 +0530

    Update AppNavigator.js

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Image} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {Image, View, ActivityIndicator} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -16,6 +16,7 @@ import TopicCardAI from '../TopicCardAI';
 import homeIcon from '../assets/homeIcon.png'
 import askIcon from '../assets/askIcon.png'
 import profileIcon from '../assets/profileIcon.png'
+import { account } from '../../constants';
 
 
 const Tab = createBottomTabNavigator();
@@ -28,12 +29,12 @@ const extraTabOptions = {
 };
 
 
-const TabNavigator = () => {
+const TabNavigator = ({user}) => {
   return (
     <Tab.Navigator>
       <Tab.Screen
         name="Home"
-        component={HomeScreen}
+        // component={HomeScreen}
         options={{
           headerShown: false,
           tabBarIcon(props) {
@@ -51,10 +52,12 @@ const TabNavigator = () => {
           },
           ...extraTabOptions,
         }}
-      />
+      >
+        {props => <HomeScreen {...props}  user={user} />}
+      </Tab.Screen>
       <Tab.Screen
         name="AskGPT"
-        component={PromptScreen}
+        // component={PromptScreen}
         options={{
           headerShown: false,
           tabBarIcon(props) {
@@ -72,10 +75,12 @@ const TabNavigator = () => {
           },
           ...extraTabOptions,
         }}
-      />
+      >
+        {props => <PromptScreen {...props}  user={user} />}
+      </Tab.Screen>
       <Tab.Screen
         name="Profile"
-        component={ProfileScreen}
+        // component={ProfileScreen}
         options={{
           headerShown: false,
           tabBarIcon(props) {
@@ -93,49 +98,118 @@ const TabNavigator = () => {
           },
           ...extraTabOptions,
         }}
-      />
-      <Tab.Screen name="Sign In" component={SignInScreen}/>
+      >
+        {props => <ProfileScreen {...props}  user={user} />}
+      </Tab.Screen>
+      {/* <Tab.Screen name="Sign In" component={SignInScreen}/> */}
     </Tab.Navigator>
   );
 };
 
-const AppNavigator = () => {
+const AppNavigator = ({user}) => {
   return (
-    <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
           name="Tabs"
-          component={TabNavigator}
+          // component={TabNavigator}
           options={{ headerShown: false }} 
-        />
+        >
+          {props => <TabNavigator {...props}  user={user} />}
+        </Stack.Screen>
         <Stack.Screen 
           name="Math AA" 
-          component={TopicCardAA}
+          // component={TopicCardAA}
           // options={{ headerShown: false }}
-        />
+        >
+          {props => <TopicCardAA {...props}  user={user} />}
+        </Stack.Screen>
         <Stack.Screen
           name="Math AI"
-          component={TopicCardAI}
+          // component={TopicCardAI}
           // options={{ headerShown: false }}
-        />
+        >
+          {props => <TopicCardAI {...props}  user={user} />}
+        </Stack.Screen>
         <Stack.Screen 
           name="QuizScreen" 
-          component={MathQuizScreen} 
+          // component={MathQuizScreen} 
           // options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="SignUp"
-          component={SignUpScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="SignIn"
-          component={SignInScreen}
-          options={{ headerShown: false }}
-        />
+        >
+          {props => <MathQuizScreen {...props}  user={user} />}
+        </Stack.Screen>
       </Stack.Navigator>
-    </NavigationContainer>
   );
 };
 
-export default AppNavigator;
+const AuthNavigator = () =>{
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="SignUp"
+        component={SignUpScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="SignIn"
+        component={SignInScreen}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  )
+}
+const MainNavigator = () => {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+
+  const checkActiveSession = async () => {
+    try {
+      const unsubscribe = await account.get();
+      console.log("User session:", unsubscribe);
+      return unsubscribe;
+    } catch (error) {
+      // console.error("Failed to get user session:", error);
+      return null;
+    }
+  }
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const verifySession = async () => {
+      try {
+        const user = await checkActiveSession();
+        if (isMounted) {
+          setUser(user ? true : false);
+        }
+      } catch (error) {
+        console.error("Error verifying session:", error);
+      } finally {
+        if (isMounted) {
+          setLoading(false);
+        }
+      }
+    };
+
+    verifySession();
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color="#1C4E80" />
+      </View>
+    );
+  }
+  
+  return (
+    <NavigationContainer>
+      {user ? <AppNavigator/> : <AuthNavigator/>}
+    </NavigationContainer>
+  )
+};
+export default MainNavigator;
